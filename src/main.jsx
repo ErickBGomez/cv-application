f292601395e58912f014a29ca4f6a899a5e3a921
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-12-18 21:48:42 -0600

    Create theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ConfigProvider, App as AntdApp } from "antd";
 import App from "./App.jsx";
+
+const appTheme = {
+  token: {
+    colorPrimary: "#426ece",
+    colorInfo: "#426ece",
+    fontSize: 16,
+    borderRadius: 8,
+    wireframe: false,
+    colorTextBase: "#2c313c",
+    colorBgBase: "#fcfcfd",
+    colorSuccess: "#43e285",
+  },
+};
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ConfigProvider theme={appTheme}>
+      <AntdApp>
+        <App />
+      </AntdApp>
+    </ConfigProvider>
   </StrictMode>
 );
