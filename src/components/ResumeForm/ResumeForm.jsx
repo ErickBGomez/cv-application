dc8a291d0122208903056d5404c3a8091f02175e
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-12-19 10:29:06 -0600

    Add contact info section

--- src/components/ResumeForm/ResumeForm.jsx
+++ src/components/ResumeForm/ResumeForm.jsx
@@ -1,5 +1,5 @@
 import { Form, Input } from "antd";
-import { FaUser } from "react-icons/fa";
+import { FaUser, FaPhoneAlt } from "react-icons/fa";
 import FormSection from "../FormSection/FormSection";
 import "./ResumeForm.scss";
 
@@ -8,7 +8,25 @@ const ResumeForm = () => {
     <Form name="resume-form" layout="vertical">
       <FormSection icon={<FaUser />} title="Personal Information">
         <Form.Item label="Full name" name="fullName" hasFeedback>
-          <Input />
+          <Input placeholder="e.g. John Doe" />
+        </Form.Item>
+
+        <Form.Item label="Work position" name="workPosition" hasFeedback>
+          <Input placeholder="e.g. Web developer" />
+        </Form.Item>
+      </FormSection>
+
+      <FormSection icon={<FaPhoneAlt />} title="Contact">
+        <Form.Item label="Email" name="email" hasFeedback>
+          <Input placeholder="e.g. johndoe@domain.com" />
+        </Form.Item>
+
+        <Form.Item label="Phone number" name="phoneNumber" hasFeedback>
+          <Input placeholder="e.g. 1234-5678" />
+        </Form.Item>
+
+        <Form.Item label="Location" name="location" hasFeedback>
+          <Input placeholder="e.g. Miami, Florida, USA" />
         </Form.Item>
       </FormSection>
     </Form>
