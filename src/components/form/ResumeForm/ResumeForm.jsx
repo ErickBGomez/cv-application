4ff8c8012cd6d777ea475fb1a2fd3c8607b197aa
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-12-25 22:34:05 -0600

    Remove comments

--- src/components/form/ResumeForm/ResumeForm.jsx
+++ src/components/form/ResumeForm/ResumeForm.jsx
@@ -4,67 +4,8 @@ import { MdBusinessCenter, MdStar } from "react-icons/md";
 import FormSection from "../FormSection/FormSection";
 import "./ResumeForm.scss";
 import DragCardsInput from "../../inputs/DragCardsInput/DragCardsInput";
-import { useState } from "react";
-
-// const initialEducationEntries = [
-//   {
-//     id: 1,
-//     title: "Bachelor of Science in Computer Science",
-//     description: "University of Miami",
-//     startYear: "2015",
-//     endYear: "2019",
-//   },
-//   {
-//     id: 2,
-//     title: "High School Diploma",
-//     description: "Miami Senior High School",
-//     startYear: "2011",
-//     endYear: "2015",
-//   },
-// ];
-
-// const initialWorkEntries = [
-//   {
-//     id: 1,
-//     title: "Web Developer",
-//     description: "Google",
-//     startYear: "2019",
-//     endYear: "Present",
-//   },
-//   {
-//     id: 2,
-//     title: "Intern",
-//     description: "Facebook",
-//     startYear: "2018",
-//     endYear: "2019",
-//   },
-// ];
-
-// const initialSkillEntries = [
-//   {
-//     id: 1,
-//     title: "JavaScript",
-//     description: 1,
-//   },
-//   {
-//     id: 2,
-//     title: "React",
-//     description: 2,
-//   },
-//   {
-//     id: 3,
-//     title: "Python",
-//     description: 3,
-//   },
-// ];
 
 const ResumeForm = () => {
-  // const [educationEntries, setEducationEntries] = useState(
-  //   initialEducationEntries
-  // );
-  // const [workEntries, setWorkEntries] = useState(initialWorkEntries);
-  // const [skillEntries, setSkillEntries] = useState(initialSkillEntries);
-
   const handleSubmit = (values) => {
     console.log(values);
   };
