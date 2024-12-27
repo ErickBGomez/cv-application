a379da7ff3d5a8423172783be32450b433d86e1c
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-12-27 16:52:44 -0600

    Create ResumeSection to avoid repetition

---
+++ src/components/resume/Resume/Resume.jsx
@@ -0,0 +1,26 @@
+import ResumeSection from "../ResumeSection/ResumeSection";
+import "./Resume.scss";
+
+const Resume = ({ data }) => {
+  const { fullname, workPosition } = data;
+
+  return (
+    <div className="resume">
+      <div className="resume-header">
+        <div className="fullname">{fullname}</div>
+        <div className="work-position">{workPosition}</div>
+      </div>
+      <aside className="resume-aside">
+        <ResumeSection title="About"></ResumeSection>
+        <ResumeSection title="Contact"></ResumeSection>
+        <ResumeSection title="Skills"></ResumeSection>
+      </aside>
+      <div className="resume-main">
+        <ResumeSection title="Education"></ResumeSection>
+        <ResumeSection title="Work Experience"></ResumeSection>
+      </div>
+    </div>
+  );
+};
+
+export default Resume;
---
+++ src/components/resume/ResumeSection/ResumeSection.jsx
@@ -0,0 +1,12 @@
+import "./ResumeSection.scss";
+
+const ResumeSection = ({ title, children }) => {
+  return (
+    <section className="resume-section">
+      <h2 className="title">title</h2>
+      {children}
+    </section>
+  );
+};
+
+export default ResumeSection;
