123e9087f902a9770eb37f9dd5238aed69c50a8d
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-12-28 17:55:08 -0600

    Add Actions buttons

--- src/pages/ResumePage/ResumePage.jsx
+++ src/pages/ResumePage/ResumePage.jsx
@@ -1,3 +1,6 @@
+import { Button } from "antd";
+import { MdEdit } from "react-icons/md";
+import { EditOutlined, PlusOutlined, PrinterOutlined } from "@ant-design/icons";
 import Resume from "../../components/resume/Resume/Resume";
 import "./ResumePage.scss";
 
@@ -56,6 +59,13 @@ const ResumePage = () => {
       <section className="title-section">
         <h1 className="title">Your Resume</h1>
         <div className="subtitle">Visualize your fantastic resume here</div>
+      </section>
+      <section className="actions">
+        <Button icon={<EditOutlined />}>Edit</Button>
+        <Button icon={<PlusOutlined />} type="primary">
+          New resume
+        </Button>
+        <Button icon={<PrinterOutlined />}>Print</Button>
       </section>
       <section className="form-section">
         <Resume data={data} />
