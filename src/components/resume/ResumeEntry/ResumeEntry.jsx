8c5bf41ada74c1266b648031a83460deb421e669
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-12-27 18:31:50 -0600

    Create ResumeEntry to show education and work experience with the same component

---
+++ src/components/resume/ResumeEntry/ResumeEntry.jsx
@@ -0,0 +1,48 @@
+const ResumeEntry = ({ context }) => {
+  const data = {
+    degree: "Bachelor's degree in Computer Science",
+    institution: "University of El Salvador",
+    startYear: 2023,
+    endYear: 2026,
+    gpa: 3.8,
+  };
+
+  const commonData = {
+    startYear: data.startYear,
+    endYear: data.endYear,
+  };
+
+  const parsedData =
+    context === "education"
+      ? {
+          ...commonData,
+          title: data.degree,
+          subtitle: data.institution,
+          description: data.gpa,
+        }
+      : {
+          ...commonData,
+          title: data.title,
+          subtitle: data.company,
+          description: data.description,
+        };
+
+  return (
+    <div className="entry">
+      <div className="title">{parsedData.title}</div>
+      <div className="subtitle">{parsedData.subtitle}</div>
+      <div className="years">
+        {parsedData.startYear} - {parsedData.endYear}
+      </div>
+      {parsedData.description && (
+        <div className="description">
+          {context === "education"
+            ? `GPA: ${parsedData.description} / 4.0`
+            : parsedData.description}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ResumeEntry;
