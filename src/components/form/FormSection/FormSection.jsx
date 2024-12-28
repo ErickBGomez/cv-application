522b589b9df1eddfc1b2ed6038646c0f62ee5c92
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-12-28 09:38:31 -0600

    Add semantic form section tags

--- src/components/form/FormSection/FormSection.jsx
+++ src/components/form/FormSection/FormSection.jsx
@@ -2,13 +2,13 @@ import "./FormSection.scss";
 
 const FormSection = ({ icon, title, children }) => {
   return (
-    <div className="section">
+    <fieldset className="section">
       <div className="title-container">
         <div className="title-icon">{icon}</div>
         <div className="title">{title}</div>
       </div>
       <div className="inputs">{children}</div>
-    </div>
+    </fieldset>
   );
 };
 
