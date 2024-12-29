76b190fbbfe22f1200cbac9c0e110412ad62131a
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-12-28 18:48:38 -0600

    Save and load data from localStorage

--- src/context/ResumeDataProvider.jsx
+++ src/context/ResumeDataProvider.jsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
+import { App } from "antd";
 import ResumeDataContext from "./ResumeDataContext";
 
 const ResumeDataProvider = ({ children }) => {
+  const { message } = App.useApp();
+
   const [data, setData] = useState([]);
 
   const saveData = (newData) => {
     setData(newData);
+    localStorage.setItem("resumeData", JSON.stringify(newData));
+  };
+
+  const loadData = () => {
+    const data = localStorage.getItem("resumeData");
+    message.success("Resume information loaded successfully");
+    return data ? JSON.parse(data) : [];
   };
 
   return (
