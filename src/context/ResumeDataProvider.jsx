38aa000d5febdb436b3014e8fe2a0a0d9ca4b63e
ErickBGomez <90425287+ErickBGomez@users.noreply.github.com>
2024-12-28 19:14:46 -0600

    Load saved data from local storage

--- src/context/ResumeDataProvider.jsx
+++ src/context/ResumeDataProvider.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { App } from "antd";
 import ResumeDataContext from "./ResumeDataContext";
 
@@ -7,15 +7,24 @@ const ResumeDataProvider = ({ children }) => {
 
   const [data, setData] = useState(null);
 
+  const loadData = () => {
+    return JSON.parse(localStorage.getItem("resumeData"));
+  };
+
+  useEffect(() => {
+    if (!data) {
+      const loaded = loadData();
+
+      if (!loaded) return;
+
+      setData(loaded);
+      message.info("Resume information recovered");
+    }
+  }, [data]);
+
   const saveData = (newData) => {
     setData(newData);
     localStorage.setItem("resumeData", JSON.stringify(newData));
-  };
-
-  const loadData = () => {
-    const data = localStorage.getItem("resumeData");
-    message.success("Resume information loaded successfully");
-    return data ? JSON.parse(data) : null;
   };
 
   return (
